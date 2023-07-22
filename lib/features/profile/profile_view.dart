74bf82c7e19e542fff171de143ac5d70f63d50e1
RazanHammad97 <razanhammadq@gmail.com>
2023-07-22 13:07:50 +0300

    finishing the profile container

--- lib/features/profile/profile_view.dart
+++ lib/features/profile/profile_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:projectbetweener/auth/widgets/custom_text_widget.dart';
 import 'package:projectbetweener/auth/widgets/screen_container.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:projectbetweener/features/profile/widgets/custom_profile_container_widget.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({Key? key}) : super(key: key);
@@ -10,15 +11,15 @@ class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenContainer(
-      body:  [
-          CustomText(
-            title: "Profile",
-            alignValue: Alignment.center,
-            fontSize: 24.sp,
-            color: Colors.black,
-          ),
-        ],
-
+      body: [
+        CustomText(
+          title: "Profile",
+          alignValue: Alignment.center,
+          fontSize: 24.sp,
+          color: Colors.black,
+        ),
+        ProfileContainer(),
+      ],
     );
   }
 }
