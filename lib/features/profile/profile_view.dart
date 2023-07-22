852e4421f0a0bc5d84e6a074ad34cda5ed84ec2e
RazanHammad97 <razanhammadq@gmail.com>
2023-07-22 13:41:19 +0300

    updating design of the profile view

--- lib/features/profile/profile_view.dart
+++ lib/features/profile/profile_view.dart
@@ -3,6 +3,7 @@ import 'package:projectbetweener/auth/widgets/custom_text_widget.dart';
 import 'package:projectbetweener/auth/widgets/screen_container.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:projectbetweener/features/profile/widgets/custom_profile_container_widget.dart';
+import 'package:projectbetweener/features/profile/widgets/custom_profile_link_widget.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({Key? key}) : super(key: key);
@@ -19,6 +20,30 @@ class ProfileView extends StatelessWidget {
           color: Colors.black,
         ),
         ProfileContainer(),
+        Column(
+          children: [
+            ProfileLink(
+              color: Color(0xFFD99696),
+              socialTitle: "Facebook".toUpperCase(),
+              socialLinkTitle: "facebook/razanhammad",
+            ),
+            ProfileLink(
+              color: Color(0xFFD9BE96),
+              socialTitle: "Facebook".toUpperCase(),
+              socialLinkTitle: "facebook/razanhammad",
+            ),
+            ProfileLink(
+              color: Color(0xFFD99696),
+              socialTitle: "Facebook".toUpperCase(),
+              socialLinkTitle: "facebook/razanhammad",
+            ),
+            ProfileLink(
+              color: Color(0xFFD9BE96),
+              socialTitle: "Facebook".toUpperCase(),
+              socialLinkTitle: "facebook/razanhammad",
+            )
+          ],
+        )
       ],
     );
   }
