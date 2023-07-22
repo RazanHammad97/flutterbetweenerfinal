0f0820a699aeb613da7ee420e70df7189e76e1fc
RazanHammad97 <razanhammadq@gmail.com>
2023-07-22 13:52:44 +0300

    preparing to design active sharing screen

--- lib/features/active-sharing/active_sharing_view.dart
+++ lib/features/active-sharing/active_sharing_view.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:projectbetweener/auth/widgets/custom_text_widget.dart';
 import 'package:projectbetweener/auth/widgets/screen_container.dart';
 
 class ActiveSharingView extends StatelessWidget {
   const ActiveSharingView({Key? key}) : super(key: key);
-
+  static String id = '/activeShare';
   @override
   Widget build(BuildContext context) {
-    return ScreenContainer(body: []);
+    return ScreenContainer(body: [
+      CustomText(title: "Active Sharing"),
+    ]);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:projectbetweener/auth/login_view.dart';
 import 'package:projectbetweener/auth/on_boarding.dart';
 import 'package:projectbetweener/core/util/general_services.dart';
+import 'package:projectbetweener/features/active-sharing/active_sharing_view.dart';
 import 'package:projectbetweener/features/home/home_view.dart';
 import 'package:projectbetweener/features/home/widgets/home_icons.dart';
 import 'auth/register_view.dart';
@@ -34,13 +35,14 @@ class MyApp extends StatelessWidget {
             textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
             scaffoldBackgroundColor: Colors.white,
           ),
-          home: const ProfileView(),
+          home: const ActiveSharingView(),
           routes: {
             RegisterView.id: (context) => const RegisterView(),
             LoginView.id: (context) => const LoginView(),
             OnBoradingView.id: (context) => const OnBoradingView(),
             HomeView.id: (context) => const HomeView(),
             ProfileView.id :(context) => const ProfileView(),
+            ActiveSharingView.id: (context)=>const ActiveSharingView(),
           },
         );
       },
