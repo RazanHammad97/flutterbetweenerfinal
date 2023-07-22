b580a735c7c76e30d4c5aadefc93b16ea67cddb7
RazanHammad97 <razanhammadq@gmail.com>
2023-07-22 13:21:17 +0300

    updating some design issues

--- lib/features/profile/widgets/custom_profile_container_widget.dart
+++ lib/features/profile/widgets/custom_profile_container_widget.dart
@@ -54,17 +54,49 @@ class ProfileContainer extends StatelessWidget {
                     leftPadding: 8,
                     color: Colors.white,
                   ),
+                  Row(
+                    children: [
+                      FollowerContainer(
+                        title: "follower 404",
+                      ),
+                      FollowerContainer(
+                        title: "following 404",
+                      ),
+                    ],
+                  )
                 ],
               ),
-              Align(
-                  alignment: Alignment.topRight
-                  ,child: Icon(Icons.edit,color: Colors.white,))
-
-
             ],
           ),
         ),
       ),
     ]);
+  }
+}
+
+class FollowerContainer extends StatelessWidget {
+  const FollowerContainer({required this.title, Key? key}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 4.0),
+      child: Container(
+        width: 80.w,
+        height: 17.h,
+        decoration: BoxDecoration(
+          color: Color(0xFFD4650F),
+          borderRadius: BorderRadius.circular(7),
+        ),
+        child: Align(
+          alignment: Alignment.center,
+          child: Text(
+            title,
+            style: TextStyle(fontSize: 12.sp),
+          ),
+        ),
+      ),
+    );
   }
 }
