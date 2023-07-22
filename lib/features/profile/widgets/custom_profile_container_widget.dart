9d5badd23df8d2e9787da4ff4899c3b13a40fedc
RazanHammad97 <razanhammadq@gmail.com>
2023-07-22 13:22:52 +0300

    seperate follower container in new file

--- lib/features/profile/widgets/custom_profile_container_widget.dart
+++ lib/features/profile/widgets/custom_profile_container_widget.dart
@@ -3,6 +3,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:projectbetweener/auth/widgets/custom_image_widget.dart';
 import 'package:projectbetweener/auth/widgets/custom_text_widget.dart';
 
+import 'follower_container.dart';
+
 class ProfileContainer extends StatelessWidget {
   const ProfileContainer({Key? key}) : super(key: key);
 
@@ -74,29 +76,4 @@ class ProfileContainer extends StatelessWidget {
   }
 }
 
-class FollowerContainer extends StatelessWidget {
-  const FollowerContainer({required this.title, Key? key}) : super(key: key);
-  final String title;
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 4.0),
-      child: Container(
-        width: 80.w,
-        height: 17.h,
-        decoration: BoxDecoration(
-          color: Color(0xFFD4650F),
-          borderRadius: BorderRadius.circular(7),
-        ),
-        child: Align(
-          alignment: Alignment.center,
-          child: Text(
-            title,
-            style: TextStyle(fontSize: 12.sp),
-          ),
-        ),
-      ),
-    );
-  }
-}
